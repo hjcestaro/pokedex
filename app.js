ae3f56fa8e8eea511adeb316031cf01ba85ef72b
Henrique Cestaro <hjcestaro@hotmail.com>
2020-06-01 18:57:47 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+const fetchPokemon = () => {
+    const pokemonPromises = []
+
+    for(let i = 1; i <= 150; i++){
+        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
+    }
+
+    Promise.all(pokemonPromises)
+    .then(pokemons => {
+        //console.log(pokemons)
+
+        const lisPokemons = pokemons.reduce((acc, pokemon) => {
+            const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+
+            acc += `
+                <li class="card ${types[0]}">
+                <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
+                   <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                   <p class="card-subtitle">${types.join(' | ')}</p>
+                </li>
+            `
+            return acc
+        }, '')
+
+        const ul = document.querySelector('[data-js="pokedex"')
+
+        ul.innerHTML = lisPokemons
+    })
+}
+
+fetchPokemon()
